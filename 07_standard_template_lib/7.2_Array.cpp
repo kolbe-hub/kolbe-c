5f08b379bfe6f66b9f48729cba11a4ce82088a2f
kolbe <me@kolbe.net>
2023-03-09 23:18:15 +0800

    添加List示例

--- 07_standard_template_lib/7.2_Array.cpp
+++ 07_standard_template_lib/7.2_Array.cpp
@@ -2,16 +2,26 @@
 #include <array>
 using namespace std;
 
+/**
+ * C++中Array的大小不支持改变，也就是不允许删除和添加元素
+ * @return
+ */
+/*
 int main() {
     array<int, 3> c = {1,2,3};
     array<int, 3> b = {2,3,4};
 
     array<int, 3>::iterator pos;
 
+    // 填充元素
+    b.fill(1);
     // 交换元素
     c.swap(b);
     for (pos = c.begin(); pos != c.end(); ++pos) {
         cout << *pos << " ";
     }
+    for (pos = b.begin(); pos != b.end(); ++pos) {
+        cout << *pos << " ";
+    }
     return 0;
-}
+}*/
